deae6eaca49217733a497854a5c1ef052b5424b7
Joell <amamihechi.udu@decagon.dev>
2020-02-08 10:22:02 +0100

    created index view

---
+++ OdeToFood.Web/Api/RestaurantController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+using OdeToFood.Data.Services;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private readonly IRestaurantData _db;
+
+        public RestaurantController(IRestaurantData db)
+        {
+            _db = db;
+        }
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+
+            var model = _db.GetAll();
+            ; return View(model);
+        }
+    }
+}
